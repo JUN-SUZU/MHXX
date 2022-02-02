b3abc35f0e29fb3ea5a00cdf8a6dea6ae18d23f7
JUN-SUZU <77867925+JUN-SUZU@users.noreply.github.com>
2022-02-02 13:30:21 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+        print "test";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>phptest</title>
+</head>
+<body>
+<?php
+$text = "HTML内でのPHP実行処理";
+echo "<p> $text </p>";
+?>
+</body>
+</html>
